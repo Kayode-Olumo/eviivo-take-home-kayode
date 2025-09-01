2cafea101fd29d3968e0afc2d96da8f8fb8b4890
Kayode-Olumo <kayode.dev@gmail.com>
2025-09-01 14:31:23 +0100

    feat: app clean up

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,10 @@
-import type React from "react"
+import React from "react"
 import type { Metadata } from "next"
 import "./globals.css"
 
 export const metadata: Metadata = {
   title: "Book App",
   description: "The best books for you",
-    generator: 'v0.app'
 }
 
 export default function RootLayout({
@@ -14,8 +13,10 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
-      <body className="antialiased font-sans">{children}</body>
+    <html lang="en-GB">
+      <body className="antialiased font-sans" suppressHydrationWarning={true}>
+        {children}
+      </body>
     </html>
   )
 }
